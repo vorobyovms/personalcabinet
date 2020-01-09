0cd05a4641f2726da5c930acbff89d56251ebb3d
vorobyov mihail <admin_instor@ukr.net>
2020-01-09 12:11:38 +0200

    09012019

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { environment } from './../environments/environment';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Авторизация</h1>
+             <p><label>Telephon <input type = text [(ngModel)]="telephone"></label></p>
+             <p><button (click)="preparaData()">Проверить телефон</button></p>
+             <p>{{ formatjson }} </p>`
 })
 export class AppComponent {
-  title = 'personalcab';
+  telephone = '';                                             // username customer
+  senddata1: any = '';                                        // structure data for json
+  formatjson: any = '';                                       // format string what send to server
+  methodAPI: any = '/CheckDataForPersonal';                   // name method check mobile telephone
+  fullurl: any = environment.server_url + this.methodAPI;     // full url to method
+  httpClientModule: HttpClientModule;
+  httpClient: HttpClient;
+
+  preparaData(): void {
+    // tslint:disable-next-line: triple-equals
+    if (this.telephone != '') {
+      // structure for json
+      this.senddata1 = { "telephone": this.telephone};
+      this.formatjson = JSON.stringify(this.senddata1);
+      this.httpClient.get(this.fullurl);
+
+    } else {
+
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
